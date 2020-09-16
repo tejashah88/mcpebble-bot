726fd9a4fa3ba4b0bf0342a10c28cc4c18f2e620
Tejas Shah <tejashah88@gmail.com>
2020-09-16 16:08:53 -0700

    added maintenance MOTD checking in collab with MaintenanceMode plugin

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import signal
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,7 +12,7 @@ from mcstatus import MinecraftServer
 SERVER_IP = os.getenv('SERVER_IP')
 mc_server = MinecraftServer.lookup(SERVER_IP)
 
-loop = asyncio.get_event_loop()
+MAINTENANCE_MOTD = os.getenv('MAINTENANCE_MOTD')
 
 bot = discord.Client()
 
@@ -20,13 +21,21 @@ async def update_status():
         mc_status = mc_server.status()
         online_ppl = mc_status.players.online
         max_ppl = mc_status.players.max
+        motd = mc_status.description['extra'][0]['text']
 
-        status = f'Online: {online_ppl}/{max_ppl} players!'
+        if MAINTENANCE_MOTD == motd:
+            status = discord.Status.do_not_disturb
+            status_msg = f'Under maintenance!'
+        else:
+            status = discord.Status.online
+            status_msg = f'Online: {online_ppl}/{max_ppl} players!'
     except:
-        status = 'Offline'
+        status_msg = 'Offline'
 
-    game = discord.Game(status)
-    await bot.change_presence(status=discord.Status.online, activity=game)
+    await bot.change_presence(
+        status=status,
+        activity=discord.Game(status_msg)
+    )
 
 
 @bot.event
@@ -60,8 +69,24 @@ async def on_message(message):
         await message.channel.send(f'The server is {main_status}')
 
 
+# Source: https://www.roguelynn.com/words/asyncio-graceful-shutdowns/
+async def shutdown(signal, loop):
+    tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
+    [task.cancel() for task in tasks]
+
+    print(f"Cancelling {len(tasks)} outstanding tasks")
+    await asyncio.gather(*tasks, return_exceptions=True)
+    loop.stop()
+
+
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     cron = Periodic(5, update_status)
+
+    signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+    for signal in signals:
+        signal_handler_fn = lambda signal=signal: asyncio.create_task(shutdown(signal, loop))
+        loop.add_signal_handler(signal, signal_handler_fn)
 
     # Shamelessly copied from https://discordpy.readthedocs.io/en/latest/api.html#discord.Client.run
     # HACK: Probably shouldn't be spamming `loop.run_until_complete`!
